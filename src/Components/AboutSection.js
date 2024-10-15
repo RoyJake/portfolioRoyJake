fe2af4c25dafbf6e636987bf7ed4057284b19952
RoyJake <royjake629@gmail.com>
2024-10-15 16:03:37 +0000

    progress

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -6,8 +6,9 @@ import './AboutSection.css';
 
 const inViewAnimation = {
  hidden: { opacity: 0, y: "100px" },
- visible: { opacity: 1, y: 0,
-  transition: { duration: 0.6}
+ visible: {
+  opacity: 1, y: 0,
+  transition: { duration: 0.6 }
  }
 }
 
@@ -22,10 +23,10 @@ const AboutSection = function () {
    <motion.div ref={ref} animate={isInView ? "visible" : "hidden"} variants={inViewAnimation} className='aboutTextWrapper'>
     <motion.h1 initial={{ opacity: 0, y: 10 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} viewport={{ amount: 0.5, once: true }}><span>01.</span>About Me</motion.h1>
     <p>
-     Hello! My name is Roy, a software engineer specializing in full-stack web development. With a robust understanding of both front-end and back-end technologies, I have the ability to craft seamless, dynamic, interactive and efficient web applications from concept to deployment.
+     Hello! My name is Roy, and I’m a software engineer with a strong interest in Artificial Intelligence, specializing in full-stack web development. With in-depth expertise in AI tools such as TensorFlow, NumPy, Pandas, and Matplotlib, alongside full-stack technologies, I have the ability to create seamless, dynamic, and efficient applications from concept to deployment.
     </p>
     <p>
-     On the front-end, I excel in bring web designs and concepts to life, creating intuitive and engaging user interfaces using modern tools and frameworks such as HTML, CSS, JavaScript, React.js, Framer Motion. My interest in design and user experience ensures that every application I build is not only functional but also delightful to use.
+     On the front-end, I excel at bringing web designs to life, crafting intuitive and engaging user interfaces with modern tools and frameworks like HTML, CSS, JavaScript/TypeScript, React.js/Next.js, and Framer Motion. My passion for design and user experience ensures that every application I build is not only highly functional but also delightful to use.
     </p>
     <p>
      On the back-end, I leverage my expertise in server-side languages and databases, including Node.js, express.js, MySQL, and MongoDB(NoSQL), to develop scalable and secure server architectures. I am adept at designing and implementing robust APIs, and ensuring data integrity and security.
@@ -36,12 +37,12 @@ const AboutSection = function () {
 
     <div className='tools'>
      <ul >
-      <li >JavaScript (ES6+)</li>
-      <li>React</li>
-      <li>Node.js</li>
+      <li>Python</li>
+      <li>Tensorflow</li>
       <li>HTML & CSS</li>
-      <li>Express.js</li>
-      <li>Framer Motion</li>
+      <li >TypeScript/JavaScript (ES6+)</li>
+      <li>Node.js</li>
+      <li>MySQL</li>
      </ul>
     </div>
    </motion.div>
